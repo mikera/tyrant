3e9a0babb895f7cd67f7f98347d5b383c9affa88
mikera <mike.r.anderson.13@gmail.com>
2012-08-31 15:19:01 +0800

    Add TyrantException

---
+++ src/main/java/mikera/tyrant/util/TyrantException.java
@@ -0,0 +1,8 @@
+package mikera.tyrant.util;
+
+@SuppressWarnings("serial")
+public class TyrantException extends RuntimeException {
+	public TyrantException(String message) {
+		super(message);
+	}
+}
